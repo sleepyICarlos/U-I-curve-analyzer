9fe1f789ada297e0fcb8dc2eea9844dfb96ee429
INSTITUT2B\hartz <felix.hartz@rwth-aachen.de>
2018-05-23 18:10:35 +0200

    new feature export_light_dark_single_files: -from multiple xls files (structured as data, calc, settings, app1, ...) select the first via option "light" and the second data trace via "dark" (light = True/False) -save all data as txt files to folder "data with(out) light" this was executed for "M12-0143\\143-1(Ar+H20)\\TLM 2" measurements.

--- excel_data_importer.py
+++ excel_data_importer.py
@@ -7,18 +7,19 @@ Created on Tue May 22 15:03:11 2018
 import pyexcel
 import pyexcel.ext.xlsx
 import numpy as  np
+import os
 
 #%% Dienstag
 """convert xls to xlsx and write txt files"""
 #set file path, and name
 path = "Z:\\sciebo\\promotion\\6_LogsDataAnalysis\\1_data\\IHT_probe_station\\"
 file_name = "Z:\\sciebo\\promotion\\6_LogsDataAnalysis\\1_data\\IHT_probe_station\\M12-0143\\data\\22-5-18_143-1_rauschen_0V_unbel-bel_retake.xls"
-folder = "Z:\\sciebo\\promotion\\6_LogsDataAnalysis\\1_data\\IHT_probe_station\\M12-0143\\143-1(Ar+H20)\\TLM 1"
-date="22-5-18"
-structure="S1"
+folder = "Z:\\sciebo\\promotion\\6_LogsDataAnalysis\\1_data\\IHT_probe_station\\M12-0143\\143-1(Ar+H20)\\TLM 2"
+date="18-5-18"
+structure="143-1"
 #%% structure
 class experiment():
-    def __init__(self, file_name, multiplicator, pads, contact_type="TLM", structure="S1", comment=[], folder =[]):
+    def __init__(self, multiplicator, pads, contact_type="TLM", structure="S1", file_name=[],  comment=[], folder =[]):
         self.file_name= file_name
         self.contact_type = contact_type
         self.structure = structure
@@ -28,8 +29,8 @@ class experiment():
         self.folder = folder
         
 area_pads=["1-1", "2-2", "3-3", "4-4", "5-5", "6-6"]
-TLM = experiment(file_name, 30, [], structure="S1")
-Areas = experiment(file_name, 1, area_pads, contact_type="Areas")
+TLM = experiment(30, [], structure="S1", file_name=file_name, folder = folder)
+Areas = experiment(1, area_pads, contact_type="Areas", file_name=file_name)
 #%% 
 """read xls, save sheet to txt-file"""
 def export_sheet_to_txt(xls_name, sheet_index, save_name):  
@@ -65,20 +66,19 @@ def txt_export(experiment):
                     save_name= "%s_%s_Areas_%f_Areas_%s.txt" % (date, experiment.structure, write_param)
                     
             export_sheet_to_txt(xls_name, sheet_index, save_name)
-            
-def txt_light_dark_single_export(experiment, folder):
-    for file in os.listdir(folder)
-    xls_name=experiment.file_name
+
+
+def light_dark_txt_export(experiment, light):   #bool True:light 1, False: dark
+    xls_name=experiment.file_name           #structure: TLM, Areas, Stripes, ...
     book = pyexcel.get_book(file_name=xls_name)
     number_of_sheets=book.number_of_sheets()
     structure_type= experiment.contact_type
-    
     for sheet_index in range(number_of_sheets):
         if 0 < sheet_index < 3:
             continue
         else:
-            print("handle sheet %d" %sheet_index)
-            if sheet_index ==0:
+            if sheet_index == 0 and light:
+                print("handle sheet %d" %sheet_index)
                 write_param = sheet_index+1
             else:
                 write_param = sheet_index-1
@@ -89,45 +89,49 @@ def txt_light_dark_single_export(experiment, folder):
             elif structure_type =="Areas":
                     write_param = experiment.pads[write_param-1];
                     save_name= "%s_%s_Areas_%f_Areas_%s.txt" % (date, experiment.structure, write_param)
-                    
+            
+            save_name= "%s\\%s" %(folder, save_name)
             export_sheet_to_txt(xls_name, sheet_index, save_name)
+            
+            
 #%%
-#my_array = pyexcel.get_array(file_name=file_name)
-#my_dict = pyexcel.get_dict(file_name=file_name)
+def light_dark_single_txt_files_export(experiment, summary_folder, measurement_index, light):
+        xls_name=experiment.file_name
+        structure_type= experiment.contact_type
+        contents = xls_name.split("_")
+        write_param=int(contents[3])
+        
+        if light==True:
+            sheet_index=0
+            data_folder=summary_folder + "\\data with light"
+            if not os.path.exists(data_folder):
+                os.makedirs(data_folder)
+        elif light==False:
+            sheet_index = 3
+            data_folder=summary_folder + "\\data without light"
+            if not os.path.exists(data_folder):
+                os.makedirs(data_folder)
 
-#pyexcel.save_book_as(file_name=file_name, dest_file_name=dest_file_name)
-#my_sheet = pyexcel.get_sheet(file_name=dest_file_name, sheet_name="Settings") #choose from {"Data", "Settings", "Append1", ...}
-#
-#
-#book = pyexcel.get_book(file_name=file_name)
-#pyexcel.save_book_as(bookdict=book, dest_file_name="converted_data.xlsx")
-#%%
-#"""load book and save sheets to single txt files"""
-#book = pyexcel.get_book(file_name=file_name)
-#names=book.sheet_names()
-#number_of_sheets=book.number_of_sheets()
+        if structure_type == "TLM":
+            save_name= "%s_%s_TLM_%d_um.txt" %(date, experiment.structure, write_param)
+        elif structure_type =="Areas":
+            write_param = experiment.pads[write_param-1];
+            save_name= "%s_%s_Areas_%f_Areas_%s.txt" % (date, experiment.structure, write_param)
+        
+        save_name= "%s\\%s" %(data_folder, save_name)
+        export_sheet_to_txt(xls_name, sheet_index, save_name)
+                
+def export_light_dark_single_files(folder, light=True):
+    index=0
+    files = []
+    summary_folder = folder
+    folder+="\\data"
+    for file in os.listdir(folder):
+        print(index)
+        files.append(file)
+        TLM = experiment(30, [], structure="143-1", file_name=file, folder = folder)
+        light_dark_single_txt_files_export(TLM, summary_folder, index, light)
+        index+=1
+    return files
 
-#convert file to python dict and then save to xlxs.
-#book_dict = pyexcel.get_book_dict(file_name=file_name)
-#pyexcel.save_book_as(bookdict=book_dict, dest_file_name="converted_data.xlsx")
-## Retrieve current working directory (`cwd`)
-#cwd = os.getcwd()
-#path = "Z:\\sciebo\\promotion\\6_LogsDataAnalysis\\1_data\\IHT_probe_station\\M12-0143\\data"
-## Change directory 
-#os.chdir(path)
-## List all files and directories in current directory
-#file=os.listdir('.')
-
-#from pyexcel._compact import OrderedDict
-#for commandline in anaconda in dir of xls files: convert to xlsx
-#$ pyexcel transcode test.xls test-out.xlsx
-
-
-##%%
-#import pyexcel as p
-#sheet = p.get_sheet(file_name = file_name, name_columns_by_row=1)
-#sheet.save_as("me.sortable.html", display_length=10)
-#from IPython.display import IFrame
-#IFrame("me.sortable.html", width = 600, height = 500)
-##sheet = pyexcel.get_sheet(file_name = file_name, name_columns_by_row=1)
-##pyexcel.save_as(file_name= file_name, dest_file_name= path+"\\felix.xlsx")
+export_light_dark_single_files(folder, light=True)
