d8dd66071f4dad2706b5b1f9d583c1118a208a5f
Felix-PC\Felix <felix.hartz@rwth-aachen.de>
2018-05-31 15:45:10 +0200

    variable names have been separated from those of init params and class definition. Code to be tested now.

--- excel_data_importer.py
+++ excel_data_importer.py
@@ -12,9 +12,9 @@ import os
         !!!user input required!!!
         precision is now set to 5 digits"""
 # adjustable parameters
-sample_folder = "M12-0143\\U-I data\\143-2 Mg+Ar mill + H2O etch\\bonded Areas"
+sample_path = "M12-0143\\U-I data\\143-2 Mg+Ar mill + H2O etch\\bonded Areas"
 
-date = "23-5-18"
+data_date = "23-5-18"
 sample_name = "143-2"
 area_pad_labels = ["6-6", "5-5", "4-4"]  # area_pad_labels=["1-1", "2-2", "3-3", "4-4", "5-5", "6-6"]
 area_pad_labels.reverse()  # optional
@@ -22,10 +22,10 @@ area_pad_labels.reverse()  # optional
 # fix parameters:
 path_office = "Z:\\sciebo\\promotion\\6_LogsDataAnalysis\\1_data\\" + "IHT_probe_station\\"
 path_home = "D:\\documents\\sciebo\\ZnSe\\wafer\\"
-precision = 5           # number of digits
+data_precision = 5           # number of digits
 
 
-class office():
+class Office():
     def __init__(self, user, work_station, folder):
         self.user = user
         self.PC = work_station
@@ -37,17 +37,17 @@ def set_path(office, sub_folder):
     return path
 
 
-home_office = office("FH", "Felix-PC", path_home)
-# uni_office = office("FH", "ag-bluhm-16", folder=path_office)
-my_sample_folder = set_path(home_office, sample_folder)
+home_office = Office("FH", "Felix-PC", path_home)
+uni_office = Office("FH", "ag-bluhm-16", folder=path_office)
+my_sample_folder = set_path(home_office, sample_path)
 print(my_sample_folder)
 my_book = my_sample_folder + "\\data\\23-5-18_143-3_Areas_highres.xls"
 print(my_book)
 #%%
 """define experiment and data set"""
-class experiment():
+class DataSet():
     def __init__(self, structure="", pad_labels=[], contact_type="TLM",
-                 xls_name="", sample_folder="", comment="", date=""):
+                 xls_name="", sample_folder="", comment="", date=data_date):
         self.xls_name = xls_name
         self.contact_type = contact_type
         self.structure = structure
@@ -59,15 +59,14 @@ class experiment():
 
 
 #%%
-TLM_data_set = experiment(structure=sample_name, xls_name=my_book, sample_folder=my_sample_folder, date=date)
+TLM_data_set = DataSet(structure=sample_name, xls_name=my_book, sample_folder=my_sample_folder)
 
 
-Area_data_set = experiment(structure=sample_name, xls_name=my_book,
+Area_data_set = DataSet(structure=sample_name, xls_name=my_book,
                            pad_labels=area_pad_labels,
-                           contact_type="Areas", sample_folder=my_sample_folder,
-                           date=date)
+                           contact_type="Areas", sample_folder=my_sample_folder)
 #%%
-def export_sheet_to_txt(xls_name, sheet_index, save_name, precision=precision):  
+def export_sheet_to_txt(xls_name, sheet_index, save_name, precision=data_precision):
     sheet_array = pyexcel.get_array(file_name=xls_name,
                                     sheet_index=sheet_index)
     precision-=1
@@ -78,14 +77,14 @@ def export_sheet_to_txt(xls_name, sheet_index, save_name, precision=precision):
                header=rows, comments="", delimiter="\t")
 #%% 
 """read single xls containing only light/dark data, save sheets to txt-file"""
-def all_sheets_txt_export(experiment, light=False): 
-    xls_name = experiment.xls_name           # structure: TLM, Areas, Stripes, ... TODO!!!
+def all_sheets_txt_export(data, light=False):
+    xls_name = data.xls_name           # structure: TLM, Areas, (STRIPES: todo)
     book = pyexcel.get_book(file_name = xls_name)
     number_of_sheets = book.number_of_sheets()
     if light:
-        export_folder = experiment.sample_folder + "\\data with light"
+        export_folder = data.sample_folder + "\\data with light"
     else:
-        export_folder = experiment.sample_folder + "\\data without light"
+        export_folder = data.sample_folder + "\\data without light"
     if not os.path.exists(export_folder):
         os.makedirs(export_folder)
         
@@ -100,21 +99,20 @@ def all_sheets_txt_export(experiment, light=False):
                 write_param = sheet_index-1
                 
             #select the right formatter depending on the measured structure:
-            structure_type= experiment.contact_type
+            structure_type= data.contact_type
             if structure_type == "TLM":
                 write_param *= 30
-                save_name = "%s_%s_TLM_%d_um.txt" % (date,
-                                                    experiment.structure,
-                                                    write_param);                                                      )
-            if structure_type == "Areas":
+                save_name = "%s_%s_TLM_%d_um.txt" % (data.date, data.structure, write_param)
+
+            elif structure_type == "Areas":
                 print("Areas", write_param);
-                write_param = experiment.pad_labels[write_param-1];
-                save_name= "%s_%s_Areas_%s.txt" % (date, experiment.structure, write_param);
+                write_param = data.pad_labels[write_param - 1]
+                save_name= "%s_%s_Areas_%s.txt" % (data.date, data.structure, write_param)
             else:
                 continue
                     
-            save_name= "%s\\%s" %(export_folder, save_name)
-            export_sheet_to_txt(xls_name, sheet_index, save_name)
+            export_name= "%s\\%s" %(export_folder, save_name)
+            export_sheet_to_txt(xls_name, sheet_index, export_name)
 
 
 #%%
@@ -122,16 +120,16 @@ def all_sheets_txt_export(experiment, light=False):
 #%% 
 """read single xls containing alternatingly light/ dark data
     save sheets to txt-file"""
-def alt_sheet_export(experiment, light=False): 
-    xls_name=experiment.xls_name           #structure: TLM, Areas, Stripes, ...
+def alt_sheet_export(data, light=False):
+    xls_name=data.xls_name           #structure: TLM, Areas, Stripes, ...
     book = pyexcel.get_book(file_name=xls_name)
     number_of_sheets=book.number_of_sheets()
 
     #set export folder
     if light:
-        export_folder=experiment.sample_folder + "\\data with light"
+        export_folder= data.sample_folder + "\\data with light"
     else:
-        export_folder=experiment.sample_folder + "\\data without light"
+        export_folder= data.sample_folder + "\\data without light"
     if not os.path.exists(export_folder):
         os.makedirs(export_folder)
         print("folder created: %s" %export_folder)
@@ -157,18 +155,16 @@ def alt_sheet_export(experiment, light=False):
             print("handle sheet %d" %sheet_index)
             
             #select the right formatter depending on the measured structure:
-            structure_type= experiment.contact_type
+            structure_type= data.contact_type
             if structure_type =="TLM":
-                    write_param *= 30
-                    save_name= "%s_%s_TLM_%d_um.txt" %(date, 
-                                                       experiment.structure,
-                                                       write_param)
+                write_param *= 30
+                save_name = "%s_%s_TLM_%d_um.txt" %(data.date, data.structure, write_param)
+
             elif structure_type =="Areas":
-                    print("Areas", write_param)
-                    write_param = experiment.pad_labels[write_param];
-                    save_name= "%s_%s_Areas_%s.txt" % (date, 
-                                                       experiment.structure,
-                                                       write_param)
+                print("Areas", write_param)
+                write_param = data.pad_labels[write_param]
+                save_name= "%s_%s_Areas_%s.txt" % (data.date, data.structure, write_param)
+
             print("save to folder %s: %s" %(export_folder, save_name))
             save_name= "%s\\%s" %(export_folder, save_name)
             export_sheet_to_txt(xls_name, sheet_index, save_name)
